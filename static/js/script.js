e37383bd32196964d7d679eb340cafb4417a33df
adelaidepillois <a.pillois@gmail.com>
2022-09-20 16:42:27 +0200

    up

--- static/js/script.js
+++ static/js/script.js
@@ -51,6 +51,7 @@ var centreYcurseur = parseInt(getComputedStyle(curseur, null).height) / 2;
 var centreXcurseur = parseInt(getComputedStyle(curseur, null).width) / 2;
 
 window.addEventListener('mousemove', e => {
+    delay: 8,
     curseur.style.left = e.pageX - centreXcurseur + "px"
     curseur.style.top = e.pageY - centreYcurseur + "px"
 
